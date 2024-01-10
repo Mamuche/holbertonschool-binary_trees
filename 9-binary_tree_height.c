07460f87b360d05ee5b988e9566ce85fbb48740a
Mamuche <marionlaroche17@gmail.com>
2024-01-10 14:51:21 +0100

    add + 1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,15 +9,19 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left = 0, right = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left = 1 + binary_tree_height(tree->left);
+
+	if (tree->right != NULL)
+		right = 1 + binary_tree_height(tree->right);
 
 	if (left > right)
-		return (1 + left);
-	return (1 + right);
+		return (left);
+
+	return (right);
 }
